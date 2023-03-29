fdbc06ef7dd6e7f627a06784841bde1ae3130c91
Raquel Manzano <rm889@cam.ac.uk>
2023-03-29 11:51:04 +0100

    Finished with filter_rna_mutations.py more stable version, added some missing filters.

--- bin/filter_rna_mutations.py
+++ bin/filter_rna_mutations.py
@@ -5,7 +5,8 @@ Script: Filters MAF file with realignment (could it run with the consensus?), no
 """
 import argparse
 import pandas as pd
-# import pysam
+from liftover import ChainFile
+from capy import mut
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -14,19 +15,24 @@ def argparser():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument("--maf", help="Input MAF file")
     parser.add_argument("--maf_2pass", help="Input MAF file after a second pass")
+    parser.add_argument('--pon19', help='Path to binary pon',
+                        default='/rds/project/rds-70r4qFasPsQ/PBCP/Work/rm889/projects/rna_mutectv2/inputs/GTeX/77759/PhenoGenotypeFiles/RootStudyConsentSet_phs000424.GTEx.v8.p2.c1.GRU/GenotypeFiles/phg000830.v1.GTEx_WES.panel-of-normals.c1/PoN_GTEx')
+    parser.add_argument('--pon', help='Path to binary pon',
+                        default='/rds/project/rds-70r4qFasPsQ/PBCP/Work/rm889/projects/rna_mutectv2/inputs/GTeX/77759/PhenoGenotypeFiles/RootStudyConsentSet_phs000424.GTEx.v8.p2.c1.GRU/GenotypeFiles/phg000830.v1.GTEx_WES.panel-of-normals.c1/PoN_GTEx')
     parser.add_argument("--whitelist", help="Input MAF file after a second pass")
     parser.add_argument("--output", help="output file name")
     parser.add_argument("--out_suffix", help="Suffix for intermidiate output", default="withRNAfilters")
     parser.add_argument('--ref',
-                        help='FASTA - out-of-date?')
-    parser.add_argument('--darned',
-                        default='')
-    parser.add_argument('--radar',
-                        default='')
-    parser.add_argument('--redi',
-                        default='')
-    parser.add_argument('--nat',
-                        default='')
+                        help='FASTA - HG38 (GDC)')
+    parser.add_argument('--ref19',
+                        help='FASTA - HG19 (hs37d5)')
+    parser.add_argument('--rnaedits',
+                        help="BED file(s) with known RNA editing events separated by space",
+                        nargs="+")
+    parser.add_argument("--thr", default=-2.8)
+    parser.add_argument('--chain', help='Chain file',
+                        default='/rds/project/rds-70r4qFasPsQ/PBCP/Work/rm889/resources/hg38ToHg19.over.chain.gz')
+
     return parser.parse_args()
 
 
@@ -48,113 +54,23 @@ def realignment(maf1, maf2):
     return maf1, maf2, maf_intersect
 
 
-def add_to_filter(current_filter, new_filter):
-    if new_filter:
-        if current_filter == "PASS":
-            current_filter = new_filter[:]
-        else:
-            current_filter += f';{new_filter}'
-    return current_filter
-
-
-def update_filter(current_filter, new_filter):
-    if current_filter != "PASS":
-        new_filter = current_filter + ";" + new_filter
-    return new_filter
-
-
-def remove_ig_and_psedo(maf, ig_list):
-    """Add IG and pseudogene filters"""
-    # fill na values
-    maf[['BIOTYPE', 'SYMBOL']] = maf[['BIOTYPE', 'SYMBOL']].fillna(value="")
-    # mask variants that were annotated as IG gene or pseudogene
-    mask_ig = (maf["SYMBOL"].isin(ig_list))
-    mask_ps = (maf["BIOTYPE"].str.contains('pseudogene'))
-    # change filter accordingly
-    maf.loc[mask_ig, 'FILTER'] = maf["FILTER"].apply(add_to_filter, args=["ig_gene"])
-    maf.loc[mask_ps, 'FILTER'] = maf["FILTER"].apply(add_to_filter, args=["pseudogene"])
-    return maf
-
-
-def filter_homopolymer(ref_context, alt, hp_length=6):
-    """Checks if the variant is in a homopolymer context"""
-    homopolymer = False
-    if len(alt) > 1:
-        alt = alt[1:]  # if insertion the check will be done
-    elif alt == "-":  # if deletion no homopolymer
-        return False
-    # we calculatate the actual length of the sequence we need to check
-    # context is as follows: flank_before:REF:flank_after (flanks are same length)
-    length_to_consider = int((len(ref_context) - 1) / 2)
-    # we substitute the ref for the alt to check the context of the variant
-    ref_context = list(ref_context)
-    ref_context[length_to_consider] = alt
-    ref_context = "".join(ref_context)
-    # only the contact that would overlap with the expected hp_length
-    # (i.e. some context does not need to be consider as it is too far)
-    context_to_consider = ref_context[length_to_consider - hp_length + 1:length_to_consider + hp_length]
-    # count the bases that are equal. If count >= hp_length it will be consider a homopolymer
-    for idx, base in enumerate(context_to_consider):
-        context_window = context_to_consider[idx:idx + hp_length]
-        if len(context_window) < hp_length:
-            break  # what is left of the sequence to check is too short
-        elif len(set(context_window)) == 1:
-            homopolymer = True  # bingo
-            break
-    return homopolymer
-
-
-def add_context(chrom, pos, ref, genome, flank=10):
-    context = genome.fetch(chrom, pos - 1 - flank, pos + flank).upper()
-    if ref != "-":  # if it is a deletion we cannot check
-        assert ref[0] == context[flank]  # check that the REF matches the context we just extracted
-    return context
-
-
-def remove_homopolymers(maf, ref):
-    """
-    Check for variants in homopolymer regions (a sequence of 6 consecutive identical bases)
-    """
-    # read genome to get context
-    genome = pysam.FastaFile(ref)
-    # Add context
-    maf["CONTEXT"] = maf.apply(lambda row: add_context(row["Chromosome"],
-                                                       row["Start_Position"],
-                                                       row["Reference_Allele"],
-                                                       genome),
-                               axis=1)
-    # add homopolymer True/False
-    maf["homopolymer"] = maf.apply(lambda row: filter_homopolymer(row["CONTEXT"],
-                                                                  row["Tumor_Seq_Allele2"]),
-                                   axis=1)
-    # change filter accordingly
-    maf.loc[maf["homopolymer"], 'FILTER'] = maf["FILTER"].apply(add_to_filter, args=["homopolymer"])
-    return maf
-
-
-def noncoding(maf, noncoding):
-    maf["noncoding"] = maf["Consequence"].apply(
-        lambda consequence: consequence.split('&')[0].split(',')[0] in noncoding)
-    # change filter accordingly
-    maf.loc[maf["noncoding"], 'FILTER'] = maf["FILTER"].apply(add_to_filter, args=["noncoding"])
-    return maf
-
-
-def remove_rnaediting(maf, database, db_name):
+def add_rnaediting_sites(maf, rnaeditingsites, realignment):
     # bed format expected
-    rnadb = pd.read_csv(database, sep="\t", names=["chr", "start", "end", "ref", "alt"], header=None)
-    rnadb["DNAchange"] = rnadb["chr"] + ":g." + \
-                         rnadb["start"].map(str) + \
-                         rnadb["ref"] + ">" + rnadb["alt"]
-    maf["rnaediting"] = maf["DNAchange"].isin(rnadb["DNAchange"])
+    maf["rnaediting"] = maf["DNAchange"].isin(rnaeditingsites["DNAchange"])
     # change filter accordingly
-
+    pon_cols = [x for x in maf.columns if "pon_thr" in x]
     for idx, row in maf.iterrows():
         ravex_filter = row["RaVeX_FILTER"].split(";")
         if ravex_filter == ["PASS"]:
             ravex_filter = []
+        if realignment:
+            if not row["realignment"]:
+                ravex_filter += ["rnaediting"]
         if row["rnaediting"]:
             ravex_filter += ["rnaediting"]
+        for pon_col in pon_cols:
+            if row[pon_col]:
+                ravex_filter += ["rna_pon" + pon_col[-5:]]
         if not ravex_filter:
             ravex_filter = ["PASS"]
         ravex_filter = ";".join(ravex_filter)
@@ -162,11 +78,51 @@ def remove_rnaediting(maf, database, db_name):
     return maf
 
 
+def extract_coords(df):
+    """Extract coordinates from maf and stores them in a bed format file"""
+    coords = df["Chromosome"] + ":" + df["Start_Position"].astype(str) + "-" + df["Start_Position"].astype(str)
+    return coords
+
+
+def run_capy(M, pon, ref, thr, chroms, suffix="_hg38"):
+    """Runs capy with hg38 PoN"""
+    print('- Running CApy' + suffix)
+    chrom = 'Chromosome'
+    start = 'Start_Position'
+    if "hg19" in suffix:
+        chrom += "19"
+        start += "19"
+    M = M[M[chrom].isin(chroms)]  # remove alt contigs
+    M['chr'] = M[chrom].str.replace('chr', '').replace('X', '23').replace('Y', '24')
+    M['pos'] = M[start]
+    M = M[M['chr'] != 'M']
+    M['n_alt'] = M["t_alt_count"]
+    M['n_ref'] = M["t_ref_count"]
+    M = M.astype({'chr': 'int32', 'pos': 'int32'})
+    M["pon_score" + suffix] = mut.filter_mutations_against_token_PoN(M=M,
+                                                                     ponfile=pon,
+                                                                     ref=ref)
+    M['pon_thr' + suffix] = M['pon_score' + suffix] >= thr
+    return M
+
+
+def add_hg19_coords_with_liftover(M, chain_file):
+    converter = ChainFile(chain_file, 'hg38', 'hg19')
+    starting_size = M.shape[0]
+    M["coordinates19"] = M.apply(lambda x: converter[x["Chromosome"]][x["Start_Position"]], axis=1)
+    tmp = pd.DataFrame(M['coordinates19'].tolist(), index=M.index)
+    tmp[['Chromosome19', 'Start_Position19', "STRAND19"]] = pd.DataFrame(tmp[0].tolist(), index=M.index)
+    liftover_size = tmp.shape[0]
+    assert starting_size == liftover_size, "Liftovered positions are not the same numer as in original MAF"
+    M = pd.concat([M, tmp], axis=1).drop(0, axis=1)
+    return M
+
+
 def write_output(args, results, output, out_suffix):
-    maf_out = args.maf.replace(".maf", f".{out_suffix}.maf").replace(".gz", "").split("/")[-1]
-    maf2_out = args.maf_2pass.replace(".maf", f".{out_suffix}.maf").replace(".gz", "").split("/")[-1]
-    maf12_out = output[:]
     if len(results.keys()) > 1:
+        maf_out = args.maf.replace(".maf", f".{out_suffix}.maf").replace(".gz", "").split("/")[-1]
+        maf2_out = args.maf_2pass.replace(".maf", f".{out_suffix}.maf").replace(".gz", "").split("/")[-1]
+        maf12_out = output[:]
         pd.concat([results[0], results[2]]).sort_values(["Chromosome",
                                                          "Start_Position"]).to_csv(maf_out,
                                                                                    sep="\t",
@@ -181,48 +137,54 @@ def write_output(args, results, output, out_suffix):
                                                                         sep="\t",
                                                                         index=False,
                                                                         header=True)
+        print(f"See:\n-{maf_out}\n-{maf2_out}\n-{maf12_out}")
     else:
-        results[0].sort_values(["Chromosome", "Start_Position"]).to_csv(maf12_out,
+        results[0].sort_values(["Chromosome", "Start_Position"]).to_csv(output,
                                                                         sep="\t",
                                                                         index=False,
                                                                         header=True)
-    print(f"See:\n-{maf_out}\n-{maf2_out}\n-{maf12_out}")
+        print(f"See: {output}")
 
 
 def main():
     args = argparser()
-    # should I do non-coding?
+    if "," in args.rnaedits[0]:
+        args.rnaedits = args.rnaedits[0].strip().split(",")
+    # chromosomes
+    chroms = [f'chr{x}' for x in list(range(1, 23)) + ['X', 'Y']]
+
     # realignment
-    # noncoding_list = ['intron_variant', 'intergenic_variant',
-    #                   'non_coding_transcript_variant', 'non_coding_transcript_exon_variant',
-    #                   'mature_miRNA_variant',
-    #                   'IGR', 'INTRON', 'RNA']
     calls_1pass = pd.read_csv(args.maf, sep="\t", comment="#")
-    # second maf from a realignment is optional
+    # If REALIGNMENT provide intersect
     if args.maf_2pass and args.maf != args.maf_2pass:
+        didrealignment = True
         calls_2pass = pd.read_csv(args.maf_2pass, sep="\t", comment="#")
         calls1, calls2, calls12 = realignment(calls_1pass, calls_2pass)
-
         calls = [calls1, calls2, calls12]
     else:
+        didrealignment = False
         calls = [calls_1pass]
-
+    # Annotate known RNA editing
     results = {}
     for idx, calls in enumerate(calls):
         if calls.empty:
             results[idx] = calls
             continue
-        # # tag noncoding
-        # noncoding(maf=calls, noncoding=noncoding_list)
-        # # tag IG and pseudo
-        # calls = remove_ig_and_psedo(maf=calls,
-        #                             ig_list=['IGH', 'IGL', 'IGK', 'TRBV', 'TRAJ', 'pseudo'])
-        # # tag homopolymers
-        # calls = remove_homopolymers(maf=calls, ref=args.ref)
-        # tag rnaediting events
-        for rnadb, namedb in zip([args.darned, args.radar, args.redi, args.nat],
-                                 ['darned', 'radar', 'redi', 'natEd']):
-            calls = remove_rnaediting(maf=calls, database=rnadb, db_name=namedb)
+        # RNA panel of normals
+        if args.pon19 and args.chain and args.ref19:
+            calls = add_hg19_coords_with_liftover(calls, chain_file=args.chain)
+            calls = run_capy(M=calls, pon=args.pon19, ref=args.ref19, thr=args.thr, suffix="_hg19", chroms=chroms)
+        calls = run_capy(M=calls, pon=args.pon, ref=args.ref, thr=args.thr, suffix="_hg38", chroms=chroms)
+        # Annotate known RNA editing
+        rnadbs = []
+        for rnadb_file in args.rnaedits:
+            rnadb = pd.read_csv(rnadb_file, sep="\t", names=["chr", "start", "end", "ref", "alt"], header=None)
+            rnadb["DNAchange"] = rnadb["chr"] + ":g." + \
+                                 rnadb["start"].map(str) + \
+                                 rnadb["ref"] + ">" + rnadb["alt"]
+            rnadbs += [rnadb]
+        rnadbs = pd.concat(rnadbs)
+        calls = add_rnaediting_sites(maf=calls, rnaeditingsites=rnadbs, realignment=didrealignment)
         results[idx] = calls
     # write maf files
     write_output(args, results, args.output, args.out_suffix)
